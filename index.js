6c64a1e0c6853171dabb3455e8eedbeb23ecd8a9
shadracknyaribo <stilespulsar77@gmail.com>
2022-11-22 09:12:23 -0800

    a

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // Write your code in this file!
+const currentUser = 'Eleven';
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
